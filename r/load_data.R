84fd9732253cb23b07cb008e881ba2f88ebf06b3
Lukas Erhard <luerhard@googlemail.com>
2021-11-13 13:36:17 +0100

    more sensible column exlusion

--- r/load_data.R
+++ r/load_data.R
@@ -3,12 +3,14 @@ library(here)
 
 load_data <- function() {
   path <- "data/ESS9e03_1.spss.zip"
-  
+
   dir <- tempdir()
-  tempfile <- unzip(path, files="ESS9e03_1.sav", exdir=dir)
+  tempfile <- unzip(path, files = "ESS9e03_1.sav", exdir = dir)
   data <- haven::read_sav(tempfile)
-  data <- subset(data, select = -c(name:proddate))
+
+  ignore_cols <- c("name", "essround", "edition", "proddate")
+  data <- data[, !names(data) %in% ignore_cols]
   unlink(dir, recursive = TRUE)
-  
+
   return(data)
 }
