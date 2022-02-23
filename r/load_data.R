005ae1d29df15932ae90a2c98f989aa5eb1cc58c
Lukas Erhard <luerhard@googlemail.com>
2022-02-23 15:07:16 +0100

    fix loading errors on laptop

--- r/load_data.R
+++ r/load_data.R
@@ -1,43 +1,25 @@
 box::use(
   haven[as_factor],
   dplyr[`%>%`, select, filter, mutate],
-  tibble[as_tibble]
-  )
+  tibble[as_tibble],
+  here[here]
+)
 
 box::use(tibble[as_tibble])
 
 
-prepare_ess1 <- function(df) {
-  df <- df %>%
-    mutate(
-      acetalv = haven::as_factor(acetalv),
-      brncntr = haven::as_factor(brncntr),
-      cntry = haven::as_factor(cntry),
-      edulvla = haven::as_factor(edulvla),
-      eimpcnt = haven::as_factor(eimpcnt),
-      eimrcnt = haven::as_factor(eimrcnt),
-      gndr = haven::as_factor(gndr),
-      hincfel = haven::as_factor(hincfel),
-      impcntr = haven::as_factor(impcntr),
-      imrcntr = haven::as_factor(imrcntr)
-    )
-  
-  return(df)
-}
-
-
 #' @export
 ess1 <- function(raw = FALSE) {
-  path <- "data/ESS1e06_6.spss.zip"
+  zipfolder <- here("data", "ESS1e06_6.spss.zip")
   filename <- "ESS1e06_6.sav"
   dir <- tempdir()
-  
-  tempfile <- utils::unzip(path, files = filename, exdir = dir)
-  df <- haven::read_sav(tempfile)
+
+  sav_file <- utils::unzip(zipfile = zipfolder, files = filename, exdir = dir)
+  df <- haven::read_sav(sav_file)
 
   ignore_cols <- c("name", "essround", "edition", "proddate")
   df <- df[, !names(df) %in% ignore_cols]
-  
+
   df <- tibble::as_tibble(df)
   return(df)
 }
